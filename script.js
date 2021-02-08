4d84f979b5e6612d0b147f5827c1b4e8e4eac98a
Fabian Vidounig <59959982+FabianVi@users.noreply.github.com>
2021-02-08 11:32:14 +0100

    pacman added + logic

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let last_known_scroll_position = 0;
 let ticking = false;
 
-var pacman = document.getElementsByClassName("pacman");
+var pacman = document.getElementsByClassName("pacman")[0];
 
 function doSomething(scroll_pos) {
   
-    if(scroll_pos>100)
-        pacman[0].style.left = scroll_pos - 100 - 440 + "px";
-    else
-        pacman[0].style.left = -440  + "px";
+    if(scroll_pos >= pacman.scrollHeight) {
+
+      let n = Math.floor((scroll_pos-pacman.scrollHeight)/(pacman.clientHeight));
+      let relative_pos = (scroll_pos - (pacman.scrollHeight + pacman.clientHeight*n));
+      let factor = (pacman.clientWidth+120) / (pacman.clientHeight);
+
+      pacman.style.top = 250 + 258*n + "px";
+
+
+      if((n%2)==0)
+      {
+        pacman.style.left = relative_pos*factor - pacman.clientWidth-120 + "px";
+        document.getElementsByClassName("pacman")[0].style.transform = "scaleX(1)";
+
+      } else {
+        document.getElementsByClassName("pacman")[0].style.transform = "scaleX(-1)";
+        pacman.style.left = window.innerWidth+120-relative_pos*factor + "px";
+      }
+
+    } else {
+      pacman.style.left = -pacman.clientWidth-120 + "px";
+    }
+
 }
 
 document.addEventListener('scroll', function(e) {
